11c71d93165eaac5dcc09ff5ce926d21606604a2
Devon Darrow <ddarrowphl@gmail.com>
2018-08-16 13:27:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,9 +3,10 @@ def square_array(numbers)
   #sq = []
   numbers.each do |element|
     numbers[index] = element * element
-    index += 1 
+    index += 1
 #  sq.push(element * element)
 
 end
-sq
+#sq
+numbers
 end
